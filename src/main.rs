ca212cdd0bfe6285baaf228fab99ebcb889d4914
Christopher Warrington <code@cw.codes>
2023-12-29 22:42:30 -0500

    Be more consistent with `use`

--- src/main.rs
+++ src/main.rs
@@ -3,26 +3,27 @@ use sha2::{Digest, Sha256};
 use std::convert::AsRef;
 use std::env;
 use std::fmt;
-use std::path::{Path, PathBuf};
-use std::{fs, io};
+use std::fs;
+use std::io;
+use std::path;
 
 type Sha256Sum = [u8; 32];
 
 struct Fingerprint {
-    pub path: PathBuf,
+    pub path: path::PathBuf,
     pub hash: Sha256Sum,
 }
 
 fn main() -> io::Result<()> {
     for argument in env::args().skip(1) {
-        let fingerprint = fingerprint_one_file(Path::new(&argument))?;
+        let fingerprint = fingerprint_one_file(path::Path::new(&argument))?;
         println!("{}", fingerprint);
     }
 
     Ok(())
 }
 
-fn fingerprint_one_file<P: AsRef<Path>>(path: P) -> io::Result<Fingerprint> {
+fn fingerprint_one_file<P: AsRef<path::Path>>(path: P) -> io::Result<Fingerprint> {
     let mut file = fs::File::open(&path)?;
     let mut hasher = Sha256::new();
 
@@ -34,7 +35,7 @@ fn fingerprint_one_file<P: AsRef<Path>>(path: P) -> io::Result<Fingerprint> {
 }
 
 impl Fingerprint {
-    fn new<P: AsRef<Path>>(path: P) -> Fingerprint {
+    fn new<P: AsRef<path::Path>>(path: P) -> Fingerprint {
         Fingerprint {
             path: path.as_ref().to_path_buf(),
             hash: Sha256Sum::default(),
